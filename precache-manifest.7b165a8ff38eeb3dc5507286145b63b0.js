184bd76da0dece4a3645e3a570d8818b1b1d571e
paolo <paolo.palmonari@monrif.net>
2020-11-01 16:59:33 +0100

    Updates

---
+++ precache-manifest.7b165a8ff38eeb3dc5507286145b63b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802df87e81333e87043bd4492d92a73f",
+    "url": "/monster.rolodex/index.html"
+  },
+  {
+    "revision": "27958c8156453fdf74b2",
+    "url": "/monster.rolodex/static/css/main.940be0a4.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster.rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster.rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27958c8156453fdf74b2",
+    "url": "/monster.rolodex/static/js/main.e6b0b2be.chunk.js"
+  },
+  {
+    "revision": "b2c09a43758e4835e489",
+    "url": "/monster.rolodex/static/js/runtime-main.1f9c7c80.js"
+  }
+]);
